f37aed2200ecb5c366428eff26b03230beb93e8a
Ryan Wooster <rwooster@umich.edu>
2014-11-25 18:39:19 -0500

    adding i2c functionality

--- main.c
+++ main.c
@@ -1,30 +1,125 @@
-#include <stdint.h>
+//#include <stdint.h>
+//
+//#include "msp430g2553.h"
+//#include "temperature_sensor.h"
+//#include "fan.h"
+//#include "i2c.h"
+//
+//
+//int main(void)
+//{
+//
+//	init_i2c(0x1111);
+//
+//	uint8_t stuff[] = {0x01};
+//	i2c_transmit_byte(stuff, 1);
+//
+//	while (1)
+//	{
+//	}
+//	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+//
+//	init_temperature_sensor(BIT5);
+//	fan_t fan = init_fan(BIT4);
+//
+//	while (1)
+//	{
+//		float temp_celsius = read_temperature_celsius();
+//
+//		if (temp_celsius > 22)
+//		{
+//			turn_on_fan(fan);
+//		}
+//		else
+//		{
+//			turn_off_fan(fan);
+//		}
+//	}
+//}
+#include <msp430g2553.h>
 
-#include "msp430g2553.h"
-#include "temperature_sensor.h"
-#include "fan.h"
+
+int TXByteCtr;
+unsigned char PRxData;
+int Rx = 0;
+char WHO_AM_I = 0x00;
+
+char itgAddress = 0x69;
+
+void init_I2C(void);
+void Transmit(void);
+void Receive(void);
 
 
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
+  P1SEL |= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
+  P1SEL2|= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
+  init_I2C();
 
 
-	init_temperature_sensor(BIT5);
-	fan_t fan = init_fan(BIT6);
 
-	while (1)
-	{
-		float temp_celsius = read_temperature_celsius();
 
-		if (temp_celsius > 22)
-		{
-			turn_on_fan(fan);
-		}
-		else
-		{
-			turn_off_fan(fan);
-		}
-	}
+  while(1){
+      //Transmit process
+      Rx = 0;
+      TXByteCtr = 1;
+      Transmit();
+      //Receive process
+      Rx = 1;
+      Receive();
+  }
 }
 
+//-------------------------------------------------------------------------------
+// The USCI_B0 data ISR is used to move received data from the I2C slave
+// to the MSP430 memory. It is structured such that it can be used to receive
+//-------------------------------------------------------------------------------
+#pragma vector = USCIAB0TX_VECTOR
+__interrupt void USCIAB0TX_ISR(void)
+{
+  if(Rx == 1){                              // Master Recieve?
+      PRxData = UCB0RXBUF;                       // Get RX data
+      __bic_SR_register_on_exit(CPUOFF);        // Exit LPM0
+  }
+
+  else{                                     // Master Transmit
+      if (TXByteCtr)                            // Check TX byte counter
+        {
+          UCB0TXBUF = WHO_AM_I;                     // Load TX buffer
+          TXByteCtr--;                            // Decrement TX byte counter
+        }
+        else
+        {
+          UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+          IFG2 &= ~UCB0TXIFG;                     // Clear USCI_B0 TX int flag
+          __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
+        }
+ }
+
+}
+void init_I2C(void) {
+      UCB0CTL1 |= UCSWRST;                      // Enable SW reset
+      UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     // I2C Master, synchronous mode
+      UCB0CTL1 = UCSSEL_2 + UCSWRST;            // Use SMCLK, keep SW reset
+      UCB0BR0 = 12;                             // fSCL = SMCLK/12 = ~100kHz
+      UCB0BR1 = 0;
+      UCB0I2CSA = itgAddress;                         // Slave Address is 069h
+      UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
+      IE2 |= UCB0RXIE + UCB0TXIE;               //Enable RX and TX interrupt
+}
+
+void Transmit(void){
+    while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+    UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
+    __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+}
+void Receive(void){
+        while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
+        UCB0CTL1 &= ~UCTR ;                     // Clear UCTR
+        UCB0CTL1 |= UCTXSTT;                    // I2C start condition
+        while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
+        UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
+        __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+}
