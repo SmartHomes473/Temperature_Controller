4188f3c1d68caae0004f6519f9c6ed6b27d25a70
Ryan Wooster <rwooster@umich.edu>
2014-11-26 21:55:39 -0500

    some i2c stuff going on in main. trying to get the lcd working. currently just trying to have the lcd acknowledge the slave address. basing code off of the lcd arduino library as the documentation

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 //#include <stdint.h>
 //
-//#include "msp430g2553.h"
+#include "msp430g2553.h"
 //#include "temperature_sensor.h"
 //#include "fan.h"
 //#include "i2c.h"
@@ -36,90 +36,38 @@
 //		}
 //	}
 //}
-#include <msp430g2553.h>
-
-
-int TXByteCtr;
-unsigned char PRxData;
-int Rx = 0;
-char WHO_AM_I = 0x00;
-
-char itgAddress = 0x69;
-
-void init_I2C(void);
-void Transmit(void);
-void Receive(void);
-
-
-int main(void)
+int main()
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
-  P1SEL |= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
-  P1SEL2|= BIT6 + BIT7;                     // Assign I2C pins to USCI_B0
-  init_I2C();
+	WDTCTL = WDTPW + WDTHOLD;
+
+    // Set up a 16MHz main clock
+    DCOCTL  = CALDCO_16MHZ;
+    BCSCTL1 = CALBC1_16MHZ;
 
 
 
+   /************
+    *I2C CONFIG*
+    ************/
 
-  while(1){
-      //Transmit process
-      Rx = 0;
-      TXByteCtr = 1;
-      Transmit();
-      //Receive process
-      Rx = 1;
-      Receive();
-  }
-}
+    // Set master mode, I2C operation, and synchronous mode
+    UCB0CTL0 = UCMST + UCMODE1 + UCMODE0 + UCSYNC;
 
-//-------------------------------------------------------------------------------
-// The USCI_B0 data ISR is used to move received data from the I2C slave
-// to the MSP430 memory. It is structured such that it can be used to receive
-//-------------------------------------------------------------------------------
-#pragma vector = USCIAB0TX_VECTOR
-__interrupt void USCIAB0TX_ISR(void)
-{
-  if(Rx == 1){                              // Master Recieve?
-      PRxData = UCB0RXBUF;                       // Get RX data
-      __bic_SR_register_on_exit(CPUOFF);        // Exit LPM0
-  }
+    UCB0CTL1 |= UCSSEL0 + UCSSEL1; // Use SMCLK
+    UCB0BR0 = 160; // Set 100kHz baud rate
+    UCB0BR1 = 0;
 
-  else{                                     // Master Transmit
-      if (TXByteCtr)                            // Check TX byte counter
-        {
-          UCB0TXBUF = WHO_AM_I;                     // Load TX buffer
-          TXByteCtr--;                            // Decrement TX byte counter
-        }
-        else
-        {
-          UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
-          IFG2 &= ~UCB0TXIFG;                     // Clear USCI_B0 TX int flag
-          __bic_SR_register_on_exit(CPUOFF);      // Exit LPM0
-        }
- }
+    UCB0I2CSA = (0x7C>>1); // Set the slave address for the nunchuck
 
-}
-void init_I2C(void) {
-      UCB0CTL1 |= UCSWRST;                      // Enable SW reset
-      UCB0CTL0 = UCMST + UCMODE_3 + UCSYNC;     // I2C Master, synchronous mode
-      UCB0CTL1 = UCSSEL_2 + UCSWRST;            // Use SMCLK, keep SW reset
-      UCB0BR0 = 12;                             // fSCL = SMCLK/12 = ~100kHz
-      UCB0BR1 = 0;
-      UCB0I2CSA = itgAddress;                         // Slave Address is 069h
-      UCB0CTL1 &= ~UCSWRST;                     // Clear SW reset, resume operation
-      IE2 |= UCB0RXIE + UCB0TXIE;               //Enable RX and TX interrupt
-}
+    // Connect the SDA/SCL pins to the I2C module
+    P1SEL  |= BIT7 + BIT6;
+    P1SEL2 |= BIT7 + BIT6;
 
-void Transmit(void){
-    while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
-    UCB0CTL1 |= UCTR + UCTXSTT;             // I2C TX, start condition
-    __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
-}
-void Receive(void){
-        while (UCB0CTL1 & UCTXSTP);             // Ensure stop condition got sent
-        UCB0CTL1 &= ~UCTR ;                     // Clear UCTR
-        UCB0CTL1 |= UCTXSTT;                    // I2C start condition
-        while (UCB0CTL1 & UCTXSTT);             // Start condition sent?
-        UCB0CTL1 |= UCTXSTP;                    // I2C stop condition
-        __bis_SR_register(CPUOFF + GIE);        // Enter LPM0 w/ interrupts
+    UCB0CTL1 &= ~UCSWRST; // Enable the USCI
+    UCB0CTL1 |= UCTR; // Set transmit mode
+    UCB0CTL1 |= UCTXSTT; // Send START + slave address
+
+    while (UCB0CTL1 & UCTXSTT);
+
+    return 0;
 }
